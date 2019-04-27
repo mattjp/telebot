f60ad793e88a36882b6f17294469131ff08858f3
mattjp <mattjp@umich.edu>
2019-04-27 12:03:32 -0400

    get request progress

--- src/steam/steam.js
+++ src/steam/steam.js
@@ -1,28 +1,48 @@
 "use strict";
 exports.__esModule = true;
-var https = require("https");
+var secrets_1 = require("../../config/secrets");
+// const https = require("https");
+var request = require("request");
 var baseUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/";
+// const queryUrl =
+// "?key=541F483EFF82A81E75E817C103F51852&format=json&steamids=76561197960287930";
 function getSteamPresence() {
-    console.log("get presence");
-    var res = getPlayerSummary();
-    // console.log(res)
-    // return res;
+    var summaries = getPlayerSummaries();
+    console.log("======");
+    console.log(summaries);
+    // summaries.forEach(summary => {
+    //   console.log("^^^^^^^^");
+    //   console.log(summary);
+    // });
 }
 exports.getSteamPresence = getSteamPresence;
-;
-// https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=541F483EFF82A81E75E817C103F51852&format=json&steamids=76561197960287930
-function getPlayerSummary() {
-    // STEAM_IDS.forEach(entry => {
-    https.get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=541F483EFF82A81E75E817C103F51852&format=json&steamids=76561197960287930", function (resp) {
-        var data = "";
-        resp.on("data", function (chunk) {
-            data += chunk;
-        });
-        resp.on("end", function () {
-            console.log(JSON.parse(data).players);
-        });
-    }).on("error", function (err) {
-        console.log("Error: " + err.message);
+function getSteamIds() {
+    var ids = "";
+    secrets_1.STEAM_IDS.forEach(function (entry) {
+        ids += entry[1] + ",";
     });
-    // });
+    // console.log(ids);
+    return ids;
+}
+function getPlayerSummaries() {
+    var queryUrl = "?key=" + secrets_1.STEAM_API_KEY + "&steamids=" + getSteamIds() + "&format=json";
+    // const summaries = request(baseUrl + queryUrl);
+    // console.log("&&&&&&");
+    // console.log(summaries.response);
+    var summaries;
+    request(baseUrl + queryUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // console.log("*****");
+            // console.log(body);
+            var jsonBody = JSON.parse(body);
+            // console.log(jsonBody);
+            summaries = jsonBody.response.players;
+            // let y = x.response;
+            // console.log(playerSummaries);
+            // return playerSummaries;
+        }
+    });
+    // console.log("*********");
+    // console.log(summaries);
+    return summaries;
 }
--- src/steam/steam.ts
+++ src/steam/steam.ts
@@ -1,39 +1,55 @@
-"use strict"
+"use strict";
 import { STEAM_API_KEY, STEAM_IDS } from "../../config/secrets";
-const https = require("https");
+// const https = require("https");
+const request = require("request");
 
 const baseUrl =
   "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/";
 
-
+// const queryUrl =
+// "?key=541F483EFF82A81E75E817C103F51852&format=json&steamids=76561197960287930";
 
 export function getSteamPresence() {
-  console.log("get presence");
-  const res = getPlayerSummary();
-  // console.log(res)
-  // return res;
-};
-
-
-
-function getPlayerSummary(): any {
-  // STEAM_IDS.forEach(entry => {
-  https.get(
-
-    (resp) => {
-      let data = "";
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-      resp.on("end", () => {
-        console.log(JSON.parse(data));
-      });
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
-
-
-
+  let summaries = getPlayerSummaries();
+  console.log("======");
+  console.log(summaries);
+  // summaries.forEach(summary => {
+  //   console.log("^^^^^^^^");
+  //   console.log(summary);
   // });
 }
 
+function getSteamIds(): String {
+  let ids = "";
+  STEAM_IDS.forEach(entry => {
+    ids += entry[1] + ",";
+  });
+  // console.log(ids);
+  return ids;
+}
+
+function getPlayerSummaries(): any {
+  const queryUrl =
+    "?key=" + STEAM_API_KEY + "&steamids=" + getSteamIds() + "&format=json";
+
+  // const summaries = request(baseUrl + queryUrl);
+  // console.log("&&&&&&");
+  // console.log(summaries.response);
+  var summaries;
+  request(baseUrl + queryUrl, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log("*****");
+      // console.log(body);
+      let jsonBody = JSON.parse(body);
+      // console.log(jsonBody);
+      summaries = jsonBody.response.players;
+      // let y = x.response;
+      // console.log(playerSummaries);
+      // return playerSummaries;
+    }
+  });
+  
+  // // console.log("*********");
+  // // console.log(summaries);
+  return summaries;
+}
